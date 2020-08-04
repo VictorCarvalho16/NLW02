7e2577db7b91e63f4bec08903a65fd77728ce142
Victor Carvalho <victor.inf2011@hotmail.com>
2020-08-04 11:02:44 -0300

    database migrations

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/users', (request, response) => {
-  return response.json('Hello World');
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' });
 });
 
 app.listen(3333);
