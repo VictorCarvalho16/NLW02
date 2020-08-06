b92d9e92adcaca6a038b11945a4b5f9fb1e7925b
Victor Carvalho <victor.inf2011@hotmail.com>
2020-08-06 16:40:43 -0300

    change color icon tabs

--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -47,7 +47,9 @@ function StudyTabs() {
         component={TeacherList}
         options={{
           tabBarLabel: 'Proffys',
-          tabBarIcon: ({ color, size }) => <Ionicons name="ios-easel" size={size} color={color} />,
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons name="ios-easel" size={size} color={focused ? '#8257e5' : color} />
+          ),
         }}
       />
       <Screen
@@ -55,7 +57,9 @@ function StudyTabs() {
         component={Favorites}
         options={{
           tabBarLabel: 'Favoritos',
-          tabBarIcon: ({ color, size }) => <Ionicons name="ios-heart" size={size} color={color} />,
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons name="ios-heart" size={size} color={focused ? '#8257e5' : color} />
+          ),
         }}
       />
     </Navigator>
