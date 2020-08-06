2de71c3a60aeb340ac1fd0b8d7f919d563cc05a7
Victor Carvalho <victor.inf2011@hotmail.com>
2020-08-06 16:30:04 -0300

    component PageHeader

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -17,6 +17,10 @@ function Landing() {
     navigation.navigate('GiveClasses');
   }
 
+  function handleNavigateToStudy() {
+    navigation.navigate('Study');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImage} style={styles.banner} />
@@ -28,7 +32,10 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <RectButton style={[styles.button, styles.buttonPrimary]}>
+        <RectButton
+          onPress={handleNavigateToStudy}
+          style={[styles.button, styles.buttonPrimary]}
+        >
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Estudar</Text>
         </RectButton>
